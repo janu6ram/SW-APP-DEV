f9e00c58dca2ac18dfdf56f1f87843ad13d27844
janu6ram <janu6ram@msitprogram.net>
2020-04-22 00:37:22 +0530

    orm with sqlalchemy part

--- project1/application.py
+++ project1/application.py
@@ -4,8 +4,8 @@ from flask import Flask, session,render_template,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from registerdb import *
 from flask_sqlalchemy import SQLAlchemy
+from registerdb import db,Users
 
 app = Flask(__name__)
 
@@ -16,8 +16,10 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
+app.secret_key = "username"
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -25,21 +27,33 @@ def index():
     return "Project 1: TODO"
 
 @app.route("/register", methods=["POST","GET"])
+@app.route("/registration.html", methods=["POST","GET"])
 def register():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
         email = request.form.get("email")
         gender = request.form.get("gender")
-        new_user=USERS(username=username,password=password,email=email,gender=gender)
-        db.session.add(new_user)
-        db.session.commit
-        print(username,password,email,gender)
-        return render_template("success.html")
-    return render_template("registration.html")
+        new_user=Users(username = username, password = password, email = email, gender = gender)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            print(username, password, email, gender)
+            message = "successfully registered"
+            return render_template("success.html",message = message)
+        except:
+            msg = "sorry your credentials are wrong. please try again"
+            return render_template("registration.html",message = msg)
+    msg = "Please fill in this form to create an account."
+    return render_template("registration.html",message = msg)
 
 @app.route("/login.html",methods=["POST","GET"])
 def login():
     if request.method == "POST":
         return render_template("success.html")
     return render_template("login.html")
+
+@app.route("/admin")
+def admin():
+    data = Users.query.all()
+    return render_template("admin.html",data=data)
--- project1/registerdb.py
+++ project1/registerdb.py
@@ -1,37 +1,20 @@
-import os
-
-from flask import Flask, session,render_template,request
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-import csv
-
-app = Flask(__name__)
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
+from datetime import datetime as dt
 # Set up database
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-class USERS(db.Model) :
-    __tablename__ = "USERS"
+class Users(db.Model) :
+    __tablename__ = "Users"
     username = db.Column(db.String, primary_key = True)
     password = db.Column(db.String, nullable = False)
     email = db.Column(db.String, nullable = True)
     gender = db.Column(db.String, nullable = False)
+    time = db.Column(db.DateTime, nullable = False)
 
-def main():
-    db.create_all()
-
-if __name__=="__main__":
-    with app.app_context():
-        main()
+    def __init__(self, username, password, email, gender):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.gender = gender
+        self.time = dt.now()
